a8a78e494087e225876815469401c09b46d27157
EmmanuelAbajo <temiabajo@gmail.com>
2020-12-28 12:52:42 +0100

    added word count

--- characters/wordcount.go
+++ characters/wordcount.go
@@ -8,6 +8,7 @@ func CountWords(text string) map[string]int {
 	words := strings.Fields(text)
 
 	for _, word := range words {
+		// accessing a map on a non existent key gives 0
 		counts[strings.ToLower(word)]++
 	}
 
